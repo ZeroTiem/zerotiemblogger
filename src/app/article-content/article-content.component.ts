b6374ed6eb25a678f45f91b20faaacfdda0bb9cd
VS_Online <maxhank22@gmail.com>
2019-12-08 01:27:03 +0800

    scss

--- src/app/article-content/article-content.component.ts
+++ src/app/article-content/article-content.component.ts
@@ -13,13 +13,15 @@ export class ArticleContentComponent implements OnInit {
   private myTemplate: any = "";
 
   ngOnInit() {
-    this.http.get("assets/新增腳本.html", { responseType: 'text' }).subscribe(htmlText => {
+    this.http.get("assets/.01 新增腳本.html", { responseType: 'text' }).subscribe(htmlText => {
       this.myTemplate = this.ConversionFormatToWeb(htmlText);
     });
   }
 
   ConversionFormatToWeb(htmlText: string) {
-    var newHtmlText = htmlText.split('<img src="').join('<img src="assets/'); // Img Src Change To Web Path
+    var newHtmlText = htmlText;
+    newHtmlText = newHtmlText.split('h1').join('h3'); // H1 Change To H3
+    newHtmlText = newHtmlText.split('<img src="').join('<img src="assets/'); // Img Src Change To Web Path
     newHtmlText = newHtmlText.replace('<html>', '').replace('</html>', '');//Remove html Tag
     var searchKeyWord = '</head>';
     var index = newHtmlText.indexOf(searchKeyWord) + searchKeyWord.length; //Get Last Head Tag Index
