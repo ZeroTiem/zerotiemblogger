544bfc88602c707acaabd5b29864f4985343564a
ZeroTiem <maxhank22@gmail.com>
2020-09-01 20:48:22 +0800

    Add Blog File And Add Banner Other

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ArticleContentComponent } from './article-content/article-content.component';
 import { HomeComponent } from './home/home.component';
 import { NETComponent } from './net/net.component';
+import { OtherComponent } from './other/other.component';
 
 const routes: Routes = [
   // { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'net', component: NETComponent },
+  { path: 'other', component: OtherComponent },
   { path: 'ArticleContent/:articleName', component: ArticleContentComponent },
   { path: 'ArticleContent/:folder/:articleName', component: ArticleContentComponent }
 ];
