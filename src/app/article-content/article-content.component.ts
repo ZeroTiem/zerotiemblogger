54dbe591e77ea6d7dcd8ae9c0b18d6253e004c41
VS_Online <maxhank22@gmail.com>
2020-09-20 20:42:32 +0800

    Modify SEO

--- src/app/article-content/article-content.component.ts
+++ src/app/article-content/article-content.component.ts
@@ -4,7 +4,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Title, Meta } from '@angular/platform-browser';
-import { PropertyWrite } from '@angular/compiler';
+import { ShareService } from '../services/share.service';
 
 @Component({
   selector: 'app-article-content',
@@ -15,9 +15,11 @@ import { PropertyWrite } from '@angular/compiler';
 export class ArticleContentComponent implements OnInit {
   constructor(
     private http: HttpClient
-    , private router: ActivatedRoute
+    , private activatedRoute: ActivatedRoute
+    , private routers: Router
     , private titleService: Title
     , private metaService: Meta
+    , private shareService: ShareService
   ) { }
 
   public myTemplate: any = "";
@@ -25,42 +27,56 @@ export class ArticleContentComponent implements OnInit {
   private folderPath: string = "";
 
   ngOnInit() {
-    var folder = this.router.snapshot.paramMap.get('folder');
+    var folder = this.activatedRoute.snapshot.paramMap.get('folder');
     this.folderPath = 'assets/';
     if (folder != null) {
       this.folderPath += folder + '/';
     }
-    var articleName = this.router.snapshot.paramMap.get('articleName');
+    var articleName = this.activatedRoute.snapshot.paramMap.get('articleName');
     this.titleService.setTitle(articleName);
     this.articleNamePath += this.folderPath + articleName + '.html';
     console.log(this.articleNamePath);
     this.http.get(this.articleNamePath, { responseType: 'text' }).subscribe(htmlText => {
-      this.myTemplate = this.ConversionFormatToWeb(htmlText, articleName);
+      this.myTemplate = this.ConversionFormatToWeb(htmlText);
+      this.shareService.setFacebookTags(
+        this.routers.url,
+        articleName,
+        // this.GetFirstText(this.myTemplate),
+        articleName,
+        this.GetMeteImg(this.myTemplate)
+      );
+
     });
   }
 
-  ConversionFormatToWeb(htmlText: string, articleName: string) {
+  ConversionFormatToWeb(htmlText: string) {
     var newHtmlText = htmlText.split('<img src="').join('<img src="' + this.folderPath); // Img Src Change To Web Path
     newHtmlText = newHtmlText.replace('<html>', '').replace('</html>', '');//Remove html Tag
     var searchKeyWord = '</head>';
     var index = newHtmlText.indexOf(searchKeyWord) + searchKeyWord.length; //Get Last Head Tag Index
     newHtmlText = newHtmlText.substring(index);
-    this.SetMeteImg(newHtmlText,articleName);
     return newHtmlText;
   }
 
-  SetMeteImg(htmlText: string, articleName: string) {
-    var htmlTextSplitImgs = htmlText.split('<img src="');
-    if (htmlTextSplitImgs != null && htmlTextSplitImgs.length > 0) {
-      var firstImgPath = htmlTextSplitImgs[1].split('"')[0];
-      console.log("MeteImg:" + firstImgPath);
-      // <meta property="og:image" content="http://xxx.xxx.com/1.jpg" />
-      this.metaService.removeTag("name='og:title'");
-      this.metaService.removeTag("name='og:image'");
-      this.metaService.addTags([
-        { name: 'og:title', content: articleName, property: 'og:title' },
-        { name: 'og:image', content: firstImgPath, property: 'og:image' },
-      ]);
+  GetMeteImg(htmlText: string) {
+    var htmlTextSplit = htmlText.split('<img src="');
+    if (htmlTextSplit != null && htmlTextSplit.length > 0) {
+      var searchText = htmlTextSplit[1].split('"')[0];
+      console.log("MeteImg:" + searchText);
+      return searchText;
     };
+    return null;
   }
+
+  // GetFirstText(htmlText: string) {
+  //   // var htmlTextSplit = htmlText.split('<font style="font-size: 14pt;">');
+  //   var htmlTextSplit = htmlText.split('<font style="font-size: 14pt;">');
+  //   if (htmlTextSplit != null && htmlTextSplit.length > 0) {
+  //     // var searchText = htmlTextSplit[1].split('<')[0];
+  //     var searchText = htmlTextSplit[2];
+  //     console.log("FirstText:" + searchText);
+  //     return searchText;
+  //   };
+  //   return null;
+  // }
 }
